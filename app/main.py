13c3f0d7b7011ecb8ca6f7517b72ed6afd47802c
nina-mir <ninamirf@gmail.com>
2024-07-18 19:08:31 -0700

    extract path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 # Uncomment this to pass the first stage
+# Another resource: 
+# https://www.codementor.io/@joaojonesventura/building-a-basic-http-server-from-scratch-in-python-1cedkg0842
+# another source for a deep dive into networking: https://hpbn.co/#toc
 import socket
+import re
 
 
 def main():
@@ -11,10 +15,27 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
 
-    connection, address = server_socket.accept()
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    while True:
+        client_connection, client_address = server_socket.accept()
+        print(client_address)
+        print(client_connection)
 
+        # Get the client request
+        request = client_connection.recv(1024).decode()
+        request_split = str(request).split("\r\n")
+        print(request_split)
+        pattern = "^GET (.+) HTTP/1.1"
+        x = re.findall(pattern, request_split[0])
+        print(x)
 
+        # Send HTTP response
+        if x[0] == '/':
+            response = b'HTTP/1.1 200 OK\r\n\r\n'
+        else:
+            response = b'HTTP/1.1 404 Not Found\r\n\r\n'
+
+        client_connection.sendall(response)
+        client_connection.close()
 
 
 if __name__ == "__main__":
