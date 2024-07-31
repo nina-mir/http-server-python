bc6176b2679ac16b98aa6b26a24e63c18a32e0c2
nina-mir <ninamirf@gmail.com>
2024-07-30 18:41:44 -0700

    concurrency implemented

--- app/main.py
+++ app/main.py
@@ -78,7 +78,7 @@ def server():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
     
 
