2132a78d4ae73781f914b123cd5bd7b0fa2f40f7
nina-mir <ninamirf@gmail.com>
2024-09-08 19:12:01 -0700

    fix test

--- app/main.py
+++ app/main.py
@@ -95,7 +95,7 @@ def construct_response(status_line, headers, response_body):
 
     # check if response is supposed to have a body in gzip
     if 'gzip' in final:            
-        result = str.encode(status_line) + b"\r\n" + str.encode(final) + str.encode(response_body)
+        result = str.encode(status_line) + b"\r\n" + str.encode(final) + str.encode(response_body) + b"\r\n"
         print("result from gzip", result)
         return result
 
